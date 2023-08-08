942f342e8e573b9df03e7ade5f79c4af552d22ae
Ricardo~ <rzgg654@gmail.com>
2023-08-08 11:27:16 -0500

    La consola muestra la ronda actual, asi como lo que la maquina escogio

--- script.js
+++ script.js
@@ -13,3 +13,51 @@ ELSE gana la maquina
 ELSE gana la maquina
 
 */
+function Rondas( jugador_seleccion, seleccion_comp_ale ) {
+    // your code here!
+    console.log("El juego tendra 5 rondas");
+function juego(){
+
+    for( let i=1; i<6; i++){
+        console.log(  "La ronda " + i + " el ganador fue:"  );
+        seleccion_compu();
+    }
+
+}
+
+juego();
+
+
+}
+
+
+let seleccion_comp_ale; 
+const jugador_seleccion = "tijera";
+Rondas( jugador_seleccion, seleccion_comp_ale);
+
+
+
+
+//Funcion que regresa piedra, papel o tijera aleatoriamente
+function seleccion_compu(){
+
+let num_aleatorio = Math.floor( Math.random() *3);
+
+switch (num_aleatorio){
+
+case 0:
+    seleccion_comp_ale = "tjera";
+    break;
+
+    case 1:
+        seleccion_comp_ale = "piedra";
+        break;
+
+        case 2:
+            seleccion_comp_ale = "papel";
+            break;
+
+}
+console.log(seleccion_comp_ale);
+}
+
