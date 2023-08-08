7ed6c5c0a2c197c8a417345e290dee27a9df99de
Ricardo~ <rzgg654@gmail.com>
2023-08-08 10:59:28 -0500

    Empece desde cero el codigo del script

--- script.js
+++ script.js
@@ -1,60 +1,15 @@
+/*
+-La computadora escoge piedra papel o tijera aleatoriamente
+-El jugador somos nosotros
+-La piedra vence a la tijera, la tijera vence al papel, y el papel a la piedra
 
-/*function playRound(seleccion_jugador, computerSelection) {
+-IF nosotros escojimos piedra Y la maquina escojio tijera, ganamos nosotros,
+ELSE gana la maquina
 
-   function game(){
+-If nosotros escojimos tijera Y la maquina escogio papel, ganamos nosotros,
+ELSE gana la maquina
 
-        console.log(("Se jugaran 5 rondas del juego"));
-        
-        if( seleccion_jugador == "papel" && computerSelection == "piedra"){
-            console.log("Gano el jugador, el papel vence a la piedra");
-        }
-        else if( seleccion_jugador == "piedra" && computerSelection == "tijera"){
-            console.log("Gana el jugador, la piedra vence a la tijera");
-        }
-        else if(seleccion_jugador == "tijera" && computerSelection == "papel"){
-            console.log("Gana el jugador, la tijera vence al papel");
-        }
-        else{
-            console.log("El jugador perdio");
-        }
-    }
+-IF nosotros escojemos papel, Y la maquina escogio piedra, ganamos nosotros,
+ELSE gana la maquina
 
-    return game();
- 
-    
-    // your code here!
-  }
-
-
-
-
- 
-  const seleccion_jugador = "tijera";
-  const computerSelection = seleccion_computadora();
-*/ 
-
-function seleccion_computadora(){
-    let numero_aleatorio = Math.floor(Math.random() *3);
- 
-
-if(numero_aleatorio == 0 ){
-    return "piedra";
-}  
-else if (numero_aleatorio == 1){
-    return "tijera";
-}
-else if (numero_aleatorio == 2){
-    return "papel";
-}
-}
-
-
-console.log( seleccion_computadora() );
-
-
-  
- // for( let i=1; i<6; i++){
-    //console.log(playRound(seleccion_jugador, computerSelection))
-//}
-//getComputerChoice();
- 
+*/
